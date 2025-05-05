36116bb232267643afa0685959a2967719932174
vickyymosafan <mvickymosafan@gmail.com>
2025-05-05 22:27:42 +0700

    refactor(user/features): remove redundant comments from index.tsx

--- resources/js/components/user/features/index.tsx
+++ resources/js/components/user/features/index.tsx
@@ -1,8 +1,3 @@
-/**
- * Features section barrel file
- * This file exports the combined FeaturesSection component
- */
-
 import FeaturesSection from './components/FeaturesSection';
 
 export default FeaturesSection;
