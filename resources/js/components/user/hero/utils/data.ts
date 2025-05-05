7cace40df592065fe6c72797dfd428e8e60cb0f9
vickyymosafan <mvickymosafan@gmail.com>
2025-05-05 13:00:21 +0700

    refactor(user/hero): update slide image URL to use local path

--- resources/js/components/user/hero/utils/data.ts
+++ resources/js/components/user/hero/utils/data.ts
@@ -26,7 +26,7 @@ export const defaultConfig: HeroConfig = {
 export const slides: Slide[] = [
     {
         id: 1,
-        image: "https://images.unsplash.com/photo-1600607687939-ce8a6c25118c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+        image: "/storage/images/hero/1.webp",
         alt: "Progressive Architecture Design",
         headline: "Spatial Vision",
         subtext: "Menghadirkan keseimbangan sempurna antara estetika dan fungsionalitas ruang",
